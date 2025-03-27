12d7f566e4182123647e8df1f38fff6c6e26e1c4
Sheila Fields <bfsolutionsinfo@gmail.com>
2025-03-26 21:05:02 -0500

    create blackjack game

---
+++ aim class 032625/blackjack/script.js
@@ -0,0 +1,14 @@
+let dealerSum = 0;
+let playerSum = 0;
+
+let dealerAceCount = 0;
+let playerAceCount = 0;
+
+let hidden;
+let deck;
+
+let canHit = true;
+
+window.onload = funtion () {
+    buildDeck();
+}
